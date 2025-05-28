eb0831e117cefe5c2243cf096149400ca805c588
Leah Wainwright <sss.shiba.medwk@gmail.com>
2025-05-28 10:57:15 +0100

    Show-html

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,7 @@ Country.destroy_all
 
 for n in 1..10 do
     User.create!(
+
         email: Faker::Internet.email(domain: 'gmail.com'),
         password: "password",
         nationality: Faker::Nation.nationality,
@@ -27,7 +28,7 @@ end
 
 for n in 1..10 do 
     Country.create!(
-        name: Faker::Address.country,
+        name: Faker::Address.unique.country,
         information: {
             About: Faker::Lorem.paragraph(sentence_count: 5),
             Culture: Faker::Lorem.paragraph(sentence_count: 5),
