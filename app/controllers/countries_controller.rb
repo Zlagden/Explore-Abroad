c68c8eafd9594f0bf75c0af1fe8bcbe551cc2c87
zakariya lagden <zlagden@gmail.com>
2025-05-28 12:45:15 +0100

    edited country show and controller

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,16 +1,9 @@
 class CountriesController < ApplicationController
-<<<<<<< HEAD
   def index
     @countries = Country.all
   end
-=======
-  
-  def index
-    @countries = Country.all
-  end
-  
+
   def show
     @country = Country.find(params[:id])
   end
->>>>>>> 100aab71d0fe751069337deb64b08d26a67c81c3
 end
