8836c792d5e718ee9b5b6d387c742f169a1c0313
Leah Wainwright <sss.shiba.medwk@gmail.com>
2025-05-28 16:13:26 +0100

    working job index & job show

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,9 @@
 class JobsController < ApplicationController
+    def index
+        @country = Country.find(params[:country_id])
+        @jobs = Job.where(country_id: params[:country_id])
+    end 
+
     def show
         @job = Job.find(params[:id])
     end
