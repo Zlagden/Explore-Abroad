0ad898f43219b55cc0932cd479f9c929c995a12f
Leah Wainwright <sss.shiba.medwk@gmail.com>
2025-05-29 14:47:16 +0100

    Added job-show styling, fixed seed-file

--- db/migrate/20250527105314_create_countries.rb
+++ db/migrate/20250527105314_create_countries.rb
@@ -2,7 +2,7 @@ class CreateCountries < ActiveRecord::Migration[7.1]
   def change
     create_table :countries do |t|
       t.string :name
-      t.text :information
+      t.json :information
       t.string :image_url
       t.float :longitude
       t.float :latitude
