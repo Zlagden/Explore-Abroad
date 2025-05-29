3bcb11aa0cfb243a3f799a785defe8d425ce731c
jewel campbell <jewelcampbell021@gmail.com>
2025-05-29 18:02:10 +0100

    mapbox globe created

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,19 @@
 class JobsController < ApplicationController
-    def show
-        @job = Job.find(params[:id])
+  skip_before_action :authenticate_user!, only: [ :index ]
+  def index
+    @jobs = Job.all
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @jobs.geocoded.map do |job|
+      {
+        lat: job.latitude,
+        lng: job.longitude
+        info_window_html: render_to_string(partial: "info_window", locals: {job: job})
+      }
+
     end
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
 end
---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,91 @@
+import { Controller } from "@hotwired/stimulus"
+import mapboxgl from 'mapbox-gl'
+
+// Connects to data-controller="map"
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+
+  connect() {
+    mapboxgl.accessToken = this.apiKeyValue
+    this.map = new mapboxgl.Map({
+        container: this.element,
+        style: 'mapbox://styles/mapbox/streets-v10',
+        projection: 'globe', // Display the map as a globe, since satellite-v9 defaults to Mercator
+    });
+
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+
+    map.addControl(new mapboxgl.NavigationControl());
+    map.scrollZoom.disable();
+
+    map.on('style.load', () => {
+        map.setFog({}); // Set the default atmosphere style
+    });
+
+    // The following values can be changed to control rotation speed:
+
+    // At low zooms, complete a revolution every two minutes.
+    const secondsPerRevolution = 240;
+    // Above zoom level 5, do not rotate.
+    const maxSpinZoom = 5;
+    // Rotate at intermediate speeds between zoom levels 3 and 5.
+    const slowSpinZoom = 3;
+
+    let userInteracting = false;
+    const spinEnabled = true;
+
+    function spinGlobe() {
+        const zoom = map.getZoom();
+        if (spinEnabled && !userInteracting && zoom < maxSpinZoom) {
+            let distancePerSecond = 360 / secondsPerRevolution;
+            if (zoom > slowSpinZoom) {
+                // Slow spinning at higher zooms
+                const zoomDif =
+                    (maxSpinZoom - zoom) / (maxSpinZoom - slowSpinZoom);
+                distancePerSecond *= zoomDif;
+            }
+            const center = map.getCenter();
+            center.lng -= distancePerSecond;
+            // Smoothly animate the map over one second.
+            // When this animation is complete, it calls a 'moveend' event.
+            map.easeTo({ center, duration: 1000, easing: (n) => n });
+        }
+    }
+
+    // Pause spinning on interaction
+    map.on('mousedown', () => {
+        userInteracting = true;
+    });
+    map.on('dragstart', () => {
+        userInteracting = true;
+    });
+
+    // When animation is complete, start spinning if there is no ongoing interaction
+    map.on('moveend', () => {
+        spinGlobe();
+    });
+
+    spinGlobe();
+  }
+  #fitMapToMarkers() {
+      const bounds = new mapboxgl.LngLatBounds()
+      this.markersValue.forEach((marker) => {
+        bounds.extend([marker.lng, marker.lat])
+      })
+      this.map.fitBounds(bounds,  { padding: 70, maxZoom: 15, duration: 0 })
+    }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(this.map);
+    })
+  }
+}
