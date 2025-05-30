65ac2c77169bb6d2e59dbe74b4ae6e7c453d454c
Leah Wainwright <sss.shiba.medwk@gmail.com>
2025-05-30 14:52:06 +0100

    job-show-page job application simple form

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,2 +1,22 @@
 class JobApplicationsController < ApplicationController
+
+    def create
+        @job_application = JobApplication.new(job_app_params)
+        @job = Job.find(params[:job_id])
+        @job_application.job = @job
+        @job_application.user = current_user
+
+
+        if @job_application.save
+            redirect_to countries_path
+        else
+            render "jobs/show", status: :unprocessable_entity
+        end
+    end
+
+    private 
+
+    def job_app_params
+        params.require(:job_application).permit(:cv, :cover_letter)
+    end
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,6 +6,7 @@ class JobsController < ApplicationController
 
     def show
         @job = Job.find(params[:id])
+        @job_application = JobApplication.new
         
     end
 end
