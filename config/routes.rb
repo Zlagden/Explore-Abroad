eeb8bd2e293b85ea2528b7ea833c672230c0fa93
jewel campbell <jewelcampbell021@gmail.com>
2025-05-30 15:54:55 +0100

    jobs styling and landing page

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :countries, only: [:index, :show] do
-    resources :jobs, only: [:index]
-  end
-  resources :jobs, only: [:index, :show] do
-    resources :job_applications, only: [:create]
+    resources :jobs, only: [:index, :show] do
+      resources :job_applications, only: [:create]
+    end
   end
 
   resources :job_applications, only: [:index, :show] do
