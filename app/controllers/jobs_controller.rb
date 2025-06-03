bd8ddc437cd20a8f01c06c112fa7da43f6edd77e
Leah Wainwright <sss.shiba.medwk@gmail.com>
2025-06-03 14:24:33 +0100

    Implement Filter&Seach, Fixed Filter&Search, Updated NavBar

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -8,6 +8,45 @@ class JobsController < ApplicationController
       @jobs = Job.all
     end
 
+    if params[:query].present?
+      @jobs = @jobs.search_by_jobs(params[:query])
+    end
+
+    if params[:salary].present?
+      @jobs = @jobs.where("description->>'Salary' > ? AND description->>'Salary' < ? ", "#{params[:salary].split("-").first}", "#{params[:salary].split("-").last}")
+    end
+
+    if [:employment_type, :seniority, :position, :field, :location].any? { |key| params[key].present? }
+      @jobs = @jobs.where("description->>'Employment_Type' = ? OR description->>'Seniority' = ? OR description->>'Postion' = ? OR description->>'Field' = ? ", "#{params[:employment_type]}", "#{params[:seniority]}", "#{params[:position]}", "#{params[:field]}")
+    end
+
+  
+
+
+
+
+    # if params[:employment_type].present?
+    #   @jobs = @jobs.where("description->>'Employment_Type' = ? ", "#{params[:employment_type]}")
+    # end
+
+    # if params[:seniority].present?
+    #   @jobs = @jobs.where("description->>'Seniority' = ? ", "#{params[:seniority]}")
+    # end
+
+    # if params[:position].present?
+    #   @jobs = @jobs.where("description->>'Postion' = ? ", "#{params[:position]}")
+    # end
+
+    # if params[:field].present?
+    #   @jobs = @jobs.where("description->>'Field' = ? ", "#{params[:field]}")
+    # end
+
+    # if params[:location].present?
+    #   @jobs = @jobs.where("'Location' = ? ", "#{params[:field]}")
+    # end
+    #raise
+  
+    
 
     # The `geocoded` scope filters only flats with coordinates
     @markers = @jobs.geocoded.map do |job|
@@ -17,6 +56,7 @@ class JobsController < ApplicationController
         info_window_html: render_to_string(partial: "info_window", locals: {job: job})
       }
     end
+
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -159,11 +159,11 @@ Country.create!([
 ])
 puts "Seeding COUNTRIES"
 
-for n in 1..20 do
+for n in 1..100 do
     Job.create!(
         company_name: Faker::Company.name,
         description: {
-            Salary: "£" + Faker::Number.between(from: 30000, to: 120000).to_s,
+            Salary: Faker::Number.between(from: 30000, to: 120000).to_i,
             Field: Faker::Job.field,
             Postion: Faker::Job.position,
             Seniority: Faker::Job.seniority,
@@ -180,7 +180,7 @@ for n in 1..20 do
             ],
         location: Faker::Address.city,
         job_title: Faker::Job.title,
-        country_id: rand(Country.first.id..Country.last.id),
+        country_id: rand(Country.first.id..Country.first.id),
         longitude: Faker::Number.normal(mean: 0, standard_deviation: 180).round(4),
         latitude: Faker::Number.normal(mean: 0, standard_deviation: 90).round(4),
     )
