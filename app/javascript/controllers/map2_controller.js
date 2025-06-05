3693548f6e3c21c307d835685885f6394695da11
jewel campbell <jewelcampbell021@gmail.com>
2025-06-05 11:47:24 +0100

    currently updating

--- app/javascript/controllers/map2_controller.js
+++ app/javascript/controllers/map2_controller.js
@@ -9,6 +9,13 @@ export default class extends Controller {
     markers: Array
   }
   connect() {
+     this.speedFactor = 30; // number of frames per longitude degree
+      this.animation; // to store and cancel the animation
+      this.startTime = 0;
+      this.progress = 0; // progress = timestamp - startTime
+      this.resetTime = false; // indicator of whether time reset is needed for the animation
+      this.pauseButton = document.getElementById('pause');
+
       mapboxgl.accessToken = this.apiKeyValue
       this.map = new mapboxgl.Map({
         container: this.element,
@@ -18,99 +25,96 @@ export default class extends Controller {
       this.#addMarkersToMap()
 
     // beginning of map
-    const geojson = {
-          'type': 'FeatureCollection',
-          'features': [
-              {
-                  'type': 'Feature',
-                  'geometry': {
-                      'type': 'LineString',
-                      'coordinates': [[0, 0]]
-                  }
+    this.geojson = {
+      'type': 'FeatureCollection',
+      'features': [
+          {
+              'type': 'Feature',
+              'geometry': {
+                  'type': 'LineString',
+                  'coordinates': [[0, 0]]
               }
-          ]
-      };
+          }
+      ]
+    };
 
-      const speedFactor = 30; // number of frames per longitude degree
-      let animation; // to store and cancel the animation
-      let startTime = 0;
-      let progress = 0; // progress = timestamp - startTime
-      let resetTime = false; // indicator of whether time reset is needed for the animation
-      const pauseButton = document.getElementById('pause');
 
-      map.on('load', () => {
-          map.addSource('line', {
-              'type': 'geojson',
-              'data': geojson
-          });
+    this.map.on('load', () => {
+        this.map.addSource('line', {
+            'type': 'geojson',
+            'data': this.geojson
+        });
 
-          // add the line which will be modified in the animation
-          map.addLayer({
-              'id': 'line-animation',
-              'type': 'line',
-              'source': 'line',
-              'layout': {
-                  'line-cap': 'round',
-                  'line-join': 'round'
-              },
-              'paint': {
-                  'line-color': '#ed6498',
-                  'line-width': 5,
-                  'line-opacity': 0.8
-              }
-          });
+        // add the line which will be modified in the animation
+        this.map.addLayer({
+            'id': 'line-animation',
+            'type': 'line',
+            'source': 'line',
+            'layout': {
+                'line-cap': 'round',
+                'line-join': 'round'
+            },
+            'paint': {
+                'line-color': '#ed6498',
+                'line-width': 5,
+                'line-opacity': 0.8
+            }
+        });
 
-          startTime = performance.now();
+      this.startTime = performance.now();
 
-          animateLine();
 
-          // click the button to pause or play
-          pauseButton.addEventListener('click', () => {
-              pauseButton.classList.toggle('pause');
-              if (pauseButton.classList.contains('pause')) {
-                  cancelAnimationFrame(animation);
+
+
+
+          // animated in a circle as a sine wave along the map.
+
+    });
+
+    this.animateLine();
+    // click the button to pause or play
+          this.pauseButton.addEventListener('click', () => {
+              this.pauseButton.classList.toggle('pause');
+              if (this.pauseButton.classList.contains('pause')) {
+                  cancelAnimationFrame(this.animation);
               } else {
-                  resetTime = true;
-                  animateLine();
+                  this.resetTime = true;
+                  this.animateLine();
               }
           });
 
           // reset startTime and progress once the tab loses or gains focus
           // requestAnimationFrame also pauses on hidden tabs by default
           document.addEventListener('visibilitychange', () => {
-              resetTime = true;
+              this.resetTime = true;
           });
 
-          // animated in a circle as a sine wave along the map.
-          function animateLine(timestamp) {
-              if (resetTime) {
-                  // resume previous progress
-                  startTime = performance.now() - progress;
-                  resetTime = false;
-              } else {
-                  progress = timestamp - startTime;
-              }
-
-              // restart if it finishes a loop
-              if (progress > speedFactor * 360) {
-                  startTime = timestamp;
-                  geojson.features[0].geometry.coordinates = [];
-              } else {
-                  const x = progress / speedFactor;
-                  // draw a sine wave with some math.
-                  const y = Math.sin((x * Math.PI) / 90) * 40;
-                  // append new coordinates to the lineString
-                  geojson.features[0].geometry.coordinates.push([x, y]);
-                  // then update the map
-                  map.getSource('line').setData(geojson);
-              }
-              // Request the next frame of the animation.
-              animation = requestAnimationFrame(animateLine);
-          }
-      });
-
-
   }
+   #animateLine() {
+            if (this.resetTime) {
+                // resume previous progress
+                this.startTime = performance.now() - this.progress;
+                this.resetTime = false;
+            } else {
+               this.progress = timestamp - this.startTime;
+            }
+
+            // restart if it finishes a loop
+            if (this.progress > this.speedFactor * 360) {
+                this.startTime = timestamp;
+                this.geojson.features[0].geometry.coordinates = [];
+            } else {
+                const x = this.progress / this.speedFactor;
+                // draw a sine wave with some math.
+                const y = Math.sin((x * Math.PI) / 90) * 40;
+                // append new coordinates to the lineString
+                this.geojson.features[0].geometry.coordinates.push([x, y]);
+                // then update the map
+                this.map.getSource('line').setData(this.geojson);
+            }
+            // Request the next frame of the animation.
+            this.animation = requestAnimationFrame(this.animateLine);
+        }
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       new mapboxgl.Marker()
